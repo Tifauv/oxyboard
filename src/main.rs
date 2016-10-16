9716c42ed070a9117cdf862963da09186f5effcc
Olivier Serve <tifauv@gmail.com>
2016-10-16 22:48:09 +0200

    Import initial

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+extern crate iron;
+extern crate router;
+
+use std::io::Read;
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+
+
+fn main() {
+	let mut router = Router::new();
+
+	router.get("/backend", backend, "backend_xml");
+	router.post("/post", post, "post_message");
+
+
+	fn backend(_: &mut Request) -> IronResult<Response> {
+		let backend_xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><board></board>";
+
+		Ok( Response::with(( status::Ok, backend_xml )))
+	}
+
+
+	fn post(request: &mut Request) -> IronResult<Response> {
+		let mut payload = String::new();
+		request.body.read_to_string(&mut payload).unwrap();
+
+		Ok( Response::with(( status::Created, "post_id" )))
+	}
+
+	Iron::new(router).http("localhost:8080").unwrap();
+}
