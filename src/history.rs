eb7805c7553ab874f837e3e48b2b6f43d428a128
Olivier Serve <tifauv@gmail.com>
2016-10-21 09:13:13 +0200

    Added constructors to post and history.

--- src/history.rs
+++ src/history.rs
@@ -4,3 +4,16 @@ use std::vec::Vec;
 pub struct History {
 	posts: Vec<Post>,
 }
+
+impl History {
+    pub fn new() -> History {
+        History {
+            posts: Vec::new()
+        }
+    }
+    
+    
+    pub fn size(&self) -> usize {
+        self.posts.len()
+    }
+}
--- src/post.rs
+++ src/post.rs
@@ -1,7 +1,23 @@
 pub struct Post {
-	id: u8,
+	id: u32,
 	timestamp: String,
 	login: String,
 	user_agent: String,
 	message: String,
+}
+
+impl Post {
+	pub fn new(p_id:u32, p_timestamp:String, p_login:String, p_user_agent:String, p_message: String) -> Post {
+		Post {
+			id: p_id,
+			timestamp: p_timestamp,
+			login: p_login,
+			user_agent: p_user_agent,
+			message: p_message,
+		}
+    }
+
+    pub fn is_authenticated(&self) -> bool {
+        self.login != ""
+    }
 }
