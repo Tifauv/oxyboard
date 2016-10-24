b84a0297a4b14a8205b734fd22499f17d1b7fc59
Olivier Serve <tifauv@gmail.com>
2016-10-24 11:48:20 +0200

    Added a first History implementation

--- src/history.rs
+++ src/history.rs
@@ -2,18 +2,29 @@ use post::Post;
 use std::vec::Vec;
 
 pub struct History {
-	posts: Vec<Post>,
+    posts: Vec<Post>,
+    maxSize: usize,
 }
 
 impl History {
-    pub fn new() -> History {
+    pub fn new(p_maxSize:usize) -> History {
         History {
-            posts: Vec::new()
+            posts: Vec::new(),
+            maxSize: p_maxSize,
         }
     }
     
     
     pub fn size(&self) -> usize {
         self.posts.len()
+    }
+    
+    
+    pub fn add(&mut self, p_post:Post) -> &mut History {
+        if (self.posts.len() >= self.maxSize) {
+            self.posts.remove(0);
+        }
+        self.posts.push(p_post);
+        self
     }
 }
