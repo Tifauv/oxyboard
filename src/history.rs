3fd6c1b1b2c6051d04492933d1e31e3ac51fe355
Olivier Serve <tifauv@gmail.com>
2016-10-27 01:24:49 +0200

    Generate the post datetime dynamically

--- src/history.rs
+++ src/history.rs
@@ -1,10 +1,13 @@
 extern crate iron;
+extern crate time;
 
-use self::iron::typemap::Key;
 use post::Message;
 use post::Post;
+use self::iron::typemap::Key;
+use self::time::{now,strftime};
 use std::slice::Iter;
 use std::vec::Vec;
+
 
 pub struct History {
 	posts: Vec<Post>,
@@ -20,20 +23,32 @@ impl History {
 			last_post_id: 1,
 		}
 	}
-    
-    
+
+
 	pub fn size(&self) -> usize {
 		self.posts.len()
 	}
-    
-    
+
+
 	pub fn add(&mut self, p_message:Message) -> u32 {
+		// Get the current time
+		let datetime = match strftime("%Y%m%d%H%M%S", &now()) {
+			Ok(x) => x,
+			Err(_) => panic!("Failed to format the current datetime as needed !"),
+		};
+
+		// Increment the post id counter
+		self.last_post_id += 1;
+
+		// Create the new Post
+		let post = Post::new(self.last_post_id, datetime, p_message);
+
+		// Remove the oldest post if the history will exceed its maximum size
 		if self.posts.len() >= self.max_size {
 			self.posts.remove(0);
 		}
-		
-		self.last_post_id += 1;
-		let post = Post::new(self.last_post_id, String::from("20161026120000"), p_message);
+
+		// Add the new post and return its id
 		self.posts.push(post);
 		self.last_post_id
 	}
