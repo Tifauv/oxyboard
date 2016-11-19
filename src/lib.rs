7ed672b5cafb245ad9ed0f47303e521eb52a916b
Olivier Serve <tifauv@gmail.com>
2016-11-19 01:36:36 +0100

    Added some documentation to the oxyboard crate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,48 @@
+/*!
+ * # Overview
+ *
+ * Oxyboard is a _tribune_, a sort of web-based chat room.
+ *
+ * # History
+ *
+ * The main structure of a chat room is the `History` which contains the messages.
+ *
+ * ## Examples
+ *
+ * To create an empty history:
+ *
+ * ```
+ * use oxyboard::history::History;
+ *
+ * let board_name = String::from("Oxyboard");
+ * let max_size = 1024;
+ * let history = History::new(&board_name, max_size);
+ * ```
+ *
+ * To load some posts from a CSV file:
+ *
+ * ```
+ * use oxyboard::history::History;
+ * use oxyboard::storage::StorageBackend;
+ * use oxyboard::storage::file_csv::CsvFileStorage;
+ *
+ * // Create the history
+ * let board_name = String::from("Oxyboard");
+ * let max_size = 1024;
+ * let mut history = History::new(&board_name, max_size);
+ *
+ * // Load a CSV file
+ * let data_dir = String::from("data");
+ * let history_file = String::from("history.csv");
+ * let storage = CsvFileStorage::new(data_dir, history_file);
+ * storage.load_history(&mut history);
+ * ```
+ *
+ * # Requests
+ *
+ * # Configuration
+ *
+ */
 extern crate csv;
 extern crate iron;
 extern crate persistent;
