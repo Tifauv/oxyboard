6d12381ce445e58f276d3ee84b1ea5e4a31b5045
Olivier Serve <tifauv@gmail.com>
2016-12-02 18:58:15 +0100

    Put the /board template into an external file instead of as a string in the code.

--- src/requests/board.rs
+++ src/requests/board.rs
@@ -77,89 +77,27 @@ impl<'a> BoardViewModel<'a> {
 
 
 /**
- * Handler for GET backend requests.
+ * Handler for GET board requests.
  *
- * Builds the XML backend and returns it.
+ * Builds the HTML page and returns it.
  */
 pub fn board_handler(p_request: &mut Request) -> IronResult<Response> {
 	// Get access to the the shared history
 	let lock = p_request.get::<State<History>>().unwrap();
 	let history = lock.read().unwrap();
 
-	// Build the backend
-    let template = mustache::compile_str("<!DOCTYPE html>
-    <html lang=\"fr\">
-    <head>
-		<meta charset=\"utf-8\">
-		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-		<title>{{board_name}}</title>
-		<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">
-		<!--[if lt IE 9]>
-		<script src=\"https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js\"></script>
-		<script src=\"https://oss.maxcdn.com/respond/1.4.2/respond.min.js\"></script>
-		<![endif]-->
-		<style>
-		#content { margin-top: 71px }
-		</style>
-    </head>
-    <body>
-		<nav class=\"navbar navbar-inverse navbar-fixed-top\">
-			<div class=\"container\">
-				<div class=\"navbar-header\">
-					<button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#navbar\" aria-expanded=\"false\" aria-controls=\"navbar\">
-						<span class=\"sr-only\">Toggle navigation</span>
-						<span class=\"icon-bar\"></span>
-						<span class=\"icon-bar\"></span>
-						<span class=\"icon-bar\"></span>
-					</button>
-					<a class=\"navbar-brand\" href=\"#\">{{board_name}}</a>
-				</div>
-				<div id=\"navbar\" class=\"collapse navbar-collapse\">
-					<ul class=\"nav navbar-nav\">
-						<li class=\"active\"><a href=\"#\">Tribune</a></li>
-						<li><a href=\"#config\">Config</a></li>
-						<li><a href=\"#\">Login</a></li>
-					</ul>
-				</div><!--/.nav-collapse -->
-			</div>
-		</nav>
+	// Compile the page template
+	// TODO: share and reuse the compiled template
+	let template = match mustache::compile_path("templates/board.mustache.html") {
+		Ok(x) => x,
+		Err(e) => return Ok( Response::with(( status::InternalServerError, format!("Template for page /board not found or not readable: {}", e) )))
+	};
 
-		<div id=\"content\" class=\"container\">
-			<div id=\"board\" class=\"col-sm-10 panel panel-default\">
-				<div class=\"panel-body\">
-					{{#posts}}
-					<div class=\"row\">
-						<div class=\"col-md-2 text-right\">
-							<span class=\"post-author\">{{user_agent}}</span>
-						</div>
-						<div class=\"col-md-1\" title=\"{{date}}\">
-							<span class=\"post-time\">{{clock}}</span>
-						</div>
-						<div class=\"col-md-9 text-justify\">
-							<span class=\"post-message\">{{message}}</span>
-						</div>
-					</div>
-					{{/posts}}
-				</div>
-			</div>
-			<div id=\"post\" class=\"col-sm-10\">
-				<form name=\"post-form\" class=\"form-inline\" method=\"post\" action=\"/post\">
-					<div class=\"form-group\">
-						<label class=\"sr-only\" for=\"message\">Message</label>
-						<div class=\"input-group\">
-							<div class=\"input-group-addon\">Anonyme</div>
-							<input type=\"text\" class=\"form-control\" id=\"message\" name=\"message\" placeholder=\"Entrez votre message ici\">
-						</div>
-						<button type=\"submit\" class=\"btn btn-primary\">Envoyer</button>
-					</div>
-				</form>
-			</div>
-		</div>
-    </body>
-    </html>").unwrap();
-
+	// Instantiate the page template
 	let mut buffer = Cursor::new(Vec::new());
 	let data = BoardViewModel::new(&history);
     template.render(&mut buffer, &data).unwrap();
+
+    // Return the page
 	Ok( Response::with(( ContentType::html().0, status::Ok, String::from_utf8(buffer.into_inner()).unwrap() )))
 }
