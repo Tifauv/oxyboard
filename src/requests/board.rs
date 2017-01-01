8f6ffa4d62b27c681b2af20247f3ab46af87f254
Olivier Serve <tifauv@gmail.com>
2017-01-01 17:43:52 +0100

    Switched from server-rendered board to client-rendering through jQuery and an ajax call to the XML backend.

--- src/requests/board.rs
+++ src/requests/board.rs
@@ -2,64 +2,11 @@
  * The handlers for the board ui.
  */
 
-use core::{ History, Post };
+use core::History;
 use iron::prelude::*;
 use mustache::MapBuilder;
 use persistent::State;
 use requests::template_engine::build_response;
-
-
-#[derive(RustcEncodable)]
-struct PostViewModel<'a> {
-	id         : u64,
-	login      : &'a str,
-	info       : String,
-	user_agent : &'a str,
-	clock      : String,
-	date       : String,
-	message    : &'a str,
-}
-
-
-impl<'a> PostViewModel<'a> {
-	fn new(p_post: &Post) -> PostViewModel {
-		PostViewModel {
-			id         : p_post.id(),
-			login      : p_post.login(),
-			info       : PostViewModel::truncate_user_agent(p_post.user_agent()),
-			user_agent : p_post.user_agent(),
-			clock      : PostViewModel::extract_clock(p_post.time()),
-			date       : PostViewModel::extract_date(p_post.time()),
-			message    : p_post.message(),
-		}
-	}
-
-
-	fn truncate_user_agent(p_user_agent: &str) -> String {
-		if p_user_agent.len() > 16 {
-			p_user_agent[.. 16].to_owned()
-		}
-		else {
-			p_user_agent.to_owned()
-		}
-	}
-
-
-	fn extract_clock(p_time: &str) -> String {
-		let hours  : &str = &p_time[ 8 .. 10];
-		let minutes: &str = &p_time[10 .. 12];
-		let seconds: &str = &p_time[12 .. 14];
-		format!("{}:{}:{}", hours, minutes, seconds)
-	}
-
-
-	fn extract_date(p_time: &str) -> String {
-		let year : &str = &p_time[0 .. 4];
-		let month: &str = &p_time[4 .. 6];
-		let day  : &str = &p_time[6 .. 8];
-		format!("{}/{}/{}", day, month, year)
-	}
-}
 
 
 /**
@@ -75,12 +22,6 @@ pub fn board_handler(p_request: &mut Request) -> IronResult<Response> {
 	let data = MapBuilder::new()
 		.insert_str("board_name", history.board_name())
 		.insert_str("current_page_board", "class=\"active\"")
-		.insert_vec("posts",      |mut builder| {
-				for post in history.iter().map(|ref p| PostViewModel::new(&p)).collect::<Vec<_>>() {
-					builder = builder.push(&post).unwrap();
-				}
-				builder
-			})
 		.build();
 
 	Ok(build_response("board.html", data))
